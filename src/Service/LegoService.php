d3711c7c3e41a1e1964d4c931ff5139721458e27
William Brooks <william.brooks@etu.unilim.fr>
2025-02-05 17:51:28 +0100

    mise à jour exo 5 du TP3

--- src/Service/LegoService.php
+++ src/Service/LegoService.php
@@ -8,7 +8,7 @@ use \PDO;
 
 class LegoService
 {
-    public function getLego(): Lego
+    public function getLegos(): array
     {
         $db = 'mysql:host=tp-symfony-mysql;dbname=lego_store;charset=utf8';
         $username = 'root';
@@ -17,16 +17,42 @@ class LegoService
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $stmt = $pdo->query('SELECT * FROM lego');
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $legos = [];
 
-        if ($row) {
-        $lego = new Lego($row['id'], $row['name'], $row['collection']);
-        $lego->setDescription($row['description']);
-        $lego->setPrice($row['price']);
-        $lego->setPieces($row['pieces']);
-        $lego->setBoxImage($row['imagebox']);
-        $lego->setLegoImage($row['imagebg']);
-        return $lego;
-        } 
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $lego = new Lego($row['id'], $row['name'], $row['collection']);
+            $lego->setDescription($row['description']);
+            $lego->setPrice($row['price']);
+            $lego->setPieces($row['pieces']);
+            $lego->setBoxImage($row['imagebox']);
+            $lego->setLegoImage($row['imagebg']);
+            $legos[] = $lego;
+        }
+
+        return $legos;
+    }
+
+    public function getLegosByCollection($collection){
+        $db = 'mysql:host=tp-symfony-mysql;dbname=lego_store;charset=utf8';
+        $username = 'root';
+        $password = 'root';
+        $pdo = new PDO($db, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $pdo->prepare('SELECT * FROM lego WHERE collection = :collection');
+        $stmt->execute(['collection' => $collection]);
+        $legos = [];
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $lego = new Lego($row['id'], $row['name'], $row['collection']);
+            $lego->setDescription($row['description']);
+            $lego->setPrice($row['price']);
+            $lego->setPieces($row['pieces']);
+            $lego->setBoxImage($row['imagebox']);
+            $lego->setLegoImage($row['imagebg']);
+            $legos[] = $lego;
+        }
+
+        return $legos;
     }
 }
