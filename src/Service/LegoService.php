08e158b3d8b56a3f781c87122eb99a674252146b
William Brooks <william.brooks@etu.unilim.fr>
2025-02-12 16:19:14 +0100

    fin exo5 du TP3

--- src/Service/LegoService.php
+++ src/Service/LegoService.php
@@ -32,15 +32,26 @@ class LegoService
         return $legos;
     }
 
-    public function getLegosByCollection($collection){
+    public function getLegosByCollection($collection)
+    {
         $db = 'mysql:host=tp-symfony-mysql;dbname=lego_store;charset=utf8';
         $username = 'root';
         $password = 'root';
         $pdo = new PDO($db, $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        // Map route names to database collection names
+        $collectionMap = [
+            'creator' => 'Creator',
+            'star_wars' => 'Star Wars',
+            'creator_expert' => 'Creator Expert',
+        ];
+
+        // Get the correct collection name from the map
+        $collectionName = $collectionMap[$collection] ?? $collection;
+
         $stmt = $pdo->prepare('SELECT * FROM lego WHERE collection = :collection');
-        $stmt->execute(['collection' => $collection]);
+        $stmt->execute(['collection' => $collectionName]);
         $legos = [];
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
